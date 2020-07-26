5750310956e19942e6ca67a4315db173c6396ba4
David <jesdavidgomez@gmail.com>
2020-07-26 12:29:16 -0500

    Adicion de codigo departamento

--- Api/api/rips.js
+++ Api/api/rips.js
@@ -2,43 +2,26 @@
 
 const db = require('../database.js');
 module.exports = (app, ruta) => {
+    app.route(ruta)
+    .get((req, res) => {
+        db.query('select "CodigoDepartamento", "Departamento", "TipoAtencion", "CapituloDX", "EsAdultoMayor", "PerteneceEtnia", "Etnia", "TieneDiscapacidad", "Discapacidad", "Total" ' + 
+                 'from vm_rips_agg', null, (err, response) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send(err);
+            } else {
+                let rows = response.rows;
+                response.rowCount > 0 ? res.json(rows) : res.status(204).send();
+            }            
+        })
+    });
     app.route(ruta+"/annomes")
     .get((req, res) => {
-        db.query('select r.anoatencion as "Anno", r.mesatencion as "Mes", r.coddptoatencion as "CodigoDepartamento", r.departamentoatencion as "Departamento", r.tipoatencion as "TipoAtencion", sum(n) as "Total" ' + 
+        db.query('select r.anoatencion as "Anno", r.mesatencion as "Mes", trim(r.coddptoatencion) as "CodigoDepartamento", trim(r.departamentoatencion) as "Departamento", r.tipoatencion as "TipoAtencion", sum(n) as "Total" ' + 
                  'from tb_rips_agg_mesanno r ' + 
+                 'where coddptoatencion not in (\'00\',\'NA\') ' + 
                  'group by r.anoatencion, r.mesatencion, r.coddptoatencion, r.departamentoatencion, r.tipoatencion ' + 
                  'order by r.anoatencion, r.mesatencion, r.coddptoatencion, r.departamentoatencion, r.tipoatencion', null, (err, response) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send(err);
-            } else {
-                let rows = response.rows;
-                response.rowCount > 0 ? res.json(rows) : res.status(204).send();
-            }            
-        })
-    });
-    app.route(ruta+"/sexo")
-    .get((req, res) => {
-        db.query('select sexo as "Sexo", count(1) as "Total" ' + 
-                 'from tb_rips ' + 
-                 'where sexo <> \'NULL\' ' + 
-                 'group by sexo ' + 
-                 'order by sexo', null, (err, response) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send(err);
-            } else {
-                let rows = response.rows;
-                response.rowCount > 0 ? res.json(rows) : res.status(204).send();
-            }            
-        })
-    });
-    app.route(ruta+"/departamentos")
-    .get((req, res) => {
-        db.query('select dptoatencion as "Departamento", count(1) as "Total" ' + 
-                 'from tb_rips ' + 
-                 'group by dptoatencion ' + 
-                 'order by dptoatencion', null, (err, response) => {
             if (err) {
                 console.error(err);
                 res.status(500).send(err);
